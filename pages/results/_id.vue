05dca649c4c8f683255912cd95ca8dddcbb81c3f
Vitaly Lyapunov <stokwell1@gmail.com>
2018-01-24 17:48:39 +0100

    store and fetching data form iTunes Api

---
+++ pages/results/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Results for {{$route.params.id}}</h1>
+    {{$store.state.albums}}
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'search'
+
+}
+</script>
